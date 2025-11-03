5b2fce3a81d577ca4bdb99fa0a7818e47a865e22
sasipreetham94-svg <sasipreetham94@gmail.com>
2025-11-03 23:05:03 +0530

    Add simple form section

---
+++ my-react-app/src/form.js
@@ -0,0 +1,16 @@
+// In src/App.js
+function App() {
+return (
+<div className="App">
+<header className="App-header">
+{/* ... existing content ... */}
+<div style={{ marginTop: '20px', border: '1px solid
+#ccc', padding: '15px' }}>
+<h2>Simple Form</h2>
+<input type="text" placeholder="Enter your name" />
+<button>Submit</button>
+</div>
+</header>
+</div>
+);
+}
